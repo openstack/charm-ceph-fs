f347a37d69782a24f472c97ea5a2e1e03f9049d7
Liam Young <liam.young@canonical.com>
2020-08-18 08:51:02 +0000

    Add Support For Erase-Coded Pools
    Add support for erasure coded pools.
    
    1) Pool name of replicated and EC pools can now be set via
       pool-name config option.
    2) Weight of replicated and EC pools can now be set via
       ceph-pool-weight config option.
    3) Charm no longer uses initialize_mds from the ceph-mds
       interface. This moves the charm inline with ceph-client
       charms where the charm explicitly creates the pools they
       need.
    4) Metadata pool name format is preserved with an underscore
       rather than a hyphen.
    
    Change-Id: I97641c6daeeb2a1a65b081201772c89f6a7f539c

--- src/reactive/ceph_fs.py
+++ src/reactive/ceph_fs.py
@@ -13,7 +13,9 @@
 # limitations under the License.
 
 from charms import reactive
-from charmhelpers.core import hookenv
+from charmhelpers.core.hookenv import (
+    service_name,
+    config)
 
 import charms_openstack.bus
 import charms_openstack.charm as charm
@@ -51,6 +53,86 @@ def config_changed():
 @reactive.when_not('ceph.create_pool.req.sent')
 @reactive.when('ceph-mds.connected')
 def storage_ceph_connected(ceph):
-    ceph.announce_mds_name()
-    ceph.initialize_mds(hookenv.service_name())
+    ceph_mds = reactive.endpoint_from_flag('ceph-mds.connected')
+    ceph_mds.announce_mds_name()
+    service = service_name()
+    weight = config('ceph-pool-weight')
+    replicas = config('ceph-osd-replication-count')
+
+    if config('rbd-pool-name'):
+        pool_name = config('rbd-pool-name')
+    else:
+        pool_name = "{}_data".format(service)
+
+    # The '_' rather than '-' in the default pool name
+    # maintains consistency with previous versions of the
+    # charm but is inconsistent with ceph-client charms.
+    metadata_pool_name = (
+        config('metadata-pool') or
+        "{}_metadata".format(service)
+    )
+    # Metadata sizing is approximately 20% of overall data weight
+    # https://ceph.io/planet/cephfs-ideal-pg-ratio-between-metadata-and-data-pools/
+    metadata_weight = weight * 0.20
+    # Resize data pool weight to accomodate metadata weight
+    weight = weight - metadata_weight
+
+    if config('pool-type') == 'erasure-coded':
+        # General EC plugin config
+        plugin = config('ec-profile-plugin')
+        technique = config('ec-profile-technique')
+        device_class = config('ec-profile-device-class')
+        bdm_k = config('ec-profile-k')
+        bdm_m = config('ec-profile-m')
+        # LRC plugin config
+        bdm_l = config('ec-profile-locality')
+        crush_locality = config('ec-profile-crush-locality')
+        # SHEC plugin config
+        bdm_c = config('ec-profile-durability-estimator')
+        # CLAY plugin config
+        bdm_d = config('ec-profile-helper-chunks')
+        scalar_mds = config('ec-profile-scalar-mds')
+        # Profile name
+        profile_name = (
+            config('ec-profile-name') or "{}-profile".format(service)
+        )
+        # Create erasure profile
+        ceph_mds.create_erasure_profile(
+            name=profile_name,
+            k=bdm_k, m=bdm_m,
+            lrc_locality=bdm_l,
+            lrc_crush_locality=crush_locality,
+            shec_durability_estimator=bdm_c,
+            clay_helper_chunks=bdm_d,
+            clay_scalar_mds=scalar_mds,
+            device_class=device_class,
+            erasure_type=plugin,
+            erasure_technique=technique
+        )
+
+        # Create EC data pool
+        ceph_mds.create_erasure_pool(
+            name=pool_name,
+            erasure_profile=profile_name,
+            weight=weight,
+            app_name=ceph_mds.ceph_pool_app_name,
+            allow_ec_overwrites=True
+        )
+        ceph_mds.create_replicated_pool(
+            name=metadata_pool_name,
+            weight=metadata_weight,
+            app_name=ceph_mds.ceph_pool_app_name
+        )
+    else:
+        ceph_mds.create_replicated_pool(
+            name=pool_name,
+            replicas=replicas,
+            weight=weight,
+            app_name=ceph_mds.ceph_pool_app_name)
+        ceph_mds.create_replicated_pool(
+            name=metadata_pool_name,
+            replicas=replicas,
+            weight=metadata_weight,
+            app_name=ceph_mds.ceph_pool_app_name)
+    ceph_mds.request_cephfs(service)
     reactive.set_state('ceph.create_pool.req.sent')
